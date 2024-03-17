f4e9b238379ae684fdc57412082295b20aad4932
Hayes <154455366+reslaid@users.noreply.github.com>
2024-03-17 22:35:17 +0200

    Update main.rs
    Changed personalization of command line arguments (using library: clap 3.0.0)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use clap::{App, Arg};
 use goblin::{Hint, peek_bytes};
-use goblin::pe::{PE, data_directories::DataDirectory};
-use scroll::{Pread, Pwrite};
-use std::env;
+use goblin::pe::PE;
+use scroll::Pread;
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process;
 
 fn extract_signature(buf: &[u8]) -> Option<&[u8]> {
@@ -97,79 +97,58 @@ fn is_pe(file: &str) -> io::Result<bool> {
     }
 }
 
-fn usage() {
-    println!(
-        "Usage: {} <source signature file (.exe/.dll)> [--pull <output certificate file (.crt)>] [--sew <input certificate file (.crt)>] [--delete] [--replace <target PE file (.exe/.dll)>] [--impl <target PE file> (.exe/.dll)]",
-        env::args().next().unwrap()
-    );
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("thief")
+        .about("A utility for manipulating digital signatures in PE structure files")
+        .arg(
+            Arg::with_name("source")
+                .help("Source PE structure file (.exe/.dll)")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("pull")
+                .long("pull")
+                .value_name("output_certificate_file")
+                .help("Extract a certificate from any file with a PE structure into .crt")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("sew")
+                .long("sew")
+                .value_name("input_certificate_file")
+                .help("Place the certificate from .crt in any PE structure file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("delete")
+                .long("delete")
+                .help("Remove certificate from PE structure file"),
+        )
+        .arg(
+            Arg::with_name("replace")
+                .long("replace")
+                .value_name("target_file")
+                .help("Embedding a digital signature from another PE structure file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("impl")
+                .long("impl")
+                .value_name("target_file")
+                .help("Embedding a digital signature from another PE structure file")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    if args.len() < 3 {
-        usage();
-        process::exit(1);
-    }
+    let source_file = matches.value_of("source").unwrap();
 
-    let source_file = &args[1];
-    
     if !is_pe(source_file).unwrap_or(false) {
         println!("Source file is not a PE executable.");
         process::exit(1);
     }
-    
-    let mut output_certificate: Option<PathBuf> = None;
-    let mut input_certificate: Option<PathBuf> = None;
-    let mut destination_file: Option<PathBuf> = None;
-    let mut should_delete: bool = false;
-    let mut destination_mode: bool = false;
 
-    let mut arg_index = 2;
-
-    while arg_index < args.len() {
-        match args[arg_index].as_str() {
-            "--impl" => {
-                if arg_index + 1 >= args.len() {
-                    println!("Missing output destination file argument for --impl option.");
-                    usage();
-                    process::exit(1);
-                }
-                destination_file = Some(PathBuf::from(&args[arg_index + 1]));
-                destination_mode = true;
-                arg_index += 2;
-            }
-            "--pull" => {
-                if arg_index + 1 >= args.len() {
-                    println!("Missing output certificate file argument for --pull option.");
-                    usage();
-                    process::exit(1);
-                }
-                output_certificate = Some(PathBuf::from(&args[arg_index + 1]));
-                arg_index += 2;
-            }
-            "--sew" => {
-                if arg_index + 1 >= args.len() {
-                    println!("Missing input certificate file argument for --sew option.");
-                    usage();
-                    process::exit(1);
-                }
-                input_certificate = Some(PathBuf::from(&args[arg_index + 1]));
-                arg_index += 2;
-            }
-            "--delete" => {
-                should_delete = true;
-                arg_index += 1;
-            }
-            _ => {
-                println!("Invalid option: {}", args[arg_index]);
-                usage();
-                process::exit(1);
-            }
-        }
-    }
-
-    if should_delete {
+    if matches.is_present("delete") {
         if let Err(e) = delete_signature(&fs::read(source_file).unwrap_or_else(|e| {
             eprintln!("Error reading source file {}: {}", source_file, e);
             process::exit(1);
@@ -177,39 +156,7 @@ fn main() {
             eprintln!("Error: {}", e);
             process::exit(1);
         }
-    } else if destination_mode {
-        let destination_out_file = destination_file.unwrap();
-
-        let signed_buf = match fs::read(source_file) {
-            Ok(buf) => buf,
-            Err(e) => {
-                eprintln!("Error reading source signature file: {}", e);
-                process::exit(1);
-            }
-        };
-    
-        let sig_data = match extract_signature(&signed_buf) {
-            Some(data) => data,
-            None => {
-                println!("Input file does not contain an Authenticode signature");
-                process::exit(1);
-            }
-        };
-    
-        let unsigned_buf = match fs::read(&destination_out_file) {
-            Ok(buf) => buf,
-            Err(e) => {
-                eprintln!("Error reading destination file: {}", e);
-                process::exit(1);
-            }
-        };
-        
-        if let Err(e) = implant_signature(&unsigned_buf, sig_data, Path::new(&destination_out_file)) {
-            eprintln!("Error: {}", e);
-            process::exit(1);
-        }
-
-    } else if let Some(output_cert_file) = output_certificate {
+    } else if let Some(output_cert_file) = matches.value_of("pull") {
         let signed_buf = match fs::read(source_file) {
             Ok(buf) => buf,
             Err(e) => {
@@ -226,18 +173,18 @@ fn main() {
             }
         };
 
-        match fs::write(&output_cert_file, sig_data) {
-            Ok(_) => println!("Signature extracted to {}", output_cert_file.to_string_lossy()),
+        match fs::write(output_cert_file, sig_data) {
+            Ok(_) => println!("Signature extracted to {}", output_cert_file),
             Err(e) => {
-                eprintln!("Error writing signature to {}: {}", output_cert_file.to_string_lossy(), e);
+                eprintln!("Error writing signature to {}: {}", output_cert_file, e);
                 process::exit(1);
             }
         }
-    } else if let Some(input_cert_file) = input_certificate {
-        let signature = match fs::read(&input_cert_file) {
+    } else if let Some(input_cert_file) = matches.value_of("sew") {
+        let signature = match fs::read(input_cert_file) {
             Ok(sig) => sig,
             Err(e) => {
-                eprintln!("Error reading input certificate file {}: {}", input_cert_file.to_string_lossy(), e);
+                eprintln!("Error reading input certificate file {}: {}", input_cert_file, e);
                 process::exit(1);
             }
         };
@@ -249,9 +196,62 @@ fn main() {
             eprintln!("Error: {}", e);
             process::exit(1);
         }
+    } else if let Some(dest_file) = matches.value_of("replace") {
+        let signed_buf = match fs::read(source_file) {
+            Ok(buf) => buf,
+            Err(e) => {
+                eprintln!("Error reading source signature file: {}", e);
+                process::exit(1);
+            }
+        };
+
+        let sig_data = match extract_signature(&signed_buf) {
+            Some(data) => data,
+            None => {
+                println!("Input file does not contain an Authenticode signature");
+                process::exit(1);
+            }
+        };
+
+        let unsigned_buf = match fs::read(dest_file) {
+            Ok(buf) => buf,
+            Err(e) => {
+                eprintln!("Error reading destination file: {}", e);
+                process::exit(1);
+            }
+        };
+
+        if let Err(e) = implant_signature(&unsigned_buf, sig_data, Path::new(dest_file)) {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }
+    } else if let Some(dest_file) = matches.value_of("impl") {
+        let dest_path = Path::new(dest_file);
+        let signed_buf = match fs::read(source_file) {
+            Ok(buf) => buf,
+            Err(e) => {
+                eprintln!("Error reading source signature file: {}", e);
+                process::exit(1);
+            }
+        };
+
+        let sig_data = match extract_signature(&signed_buf) {
+            Some(data) => data,
+            None => {
+                println!("Input file does not contain an Authenticode signature");
+                process::exit(1);
+            }
+        };
+
+        if let Err(e) = implant_signature(&fs::read(dest_path).unwrap_or_else(|e| {
+            eprintln!("Error reading destination file {}: {}", dest_path.display(), e);
+            process::exit(1);
+        }), &sig_data, dest_path) {
+            eprintln!("Error: {}", e);
+            process::exit(1);
+        }
     } else {
         println!("Invalid arguments.");
-        usage();
         process::exit(1);
     }
 }
